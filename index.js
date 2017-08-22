a85e24cb4499607cbdb5c74143d096a5050f7711
Aaron Kau <aaron.kau@rutgers.edu>
2017-08-22 18:30:56 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Serve static files from React app.
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//Put all API endpoints under '/API'
+app.get('/api', (req, res) => {
+	res.json("Hello");
+});
+
+//Anything else will send back React's index.html file.
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on port ${port}`);
