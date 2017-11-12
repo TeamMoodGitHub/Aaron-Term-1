709fc984e0015af2d9e7295846b739e9afa0c616
Aaron Kau <aaron.kau@rutgers.edu>
2017-11-12 17:42:12 -0500

    Add Route Builder Page.

---
+++ client/src/pages/routeBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RouteBuilder extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <h1>Test</h1>;
+	}
+
+}
+
+export default RouteBuilder;
