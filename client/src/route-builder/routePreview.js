fb0acb4d62f68825fe9d971ecf2fbb325c9cb9b3
Aaron Kau <aaron.kau@rutgers.edu>
2017-12-06 01:43:25 -0500

    Add Scroll Bar to Jungle Route Preview.

--- client/src/route-builder/routePreview.js
+++ client/src/route-builder/routePreview.js
@@ -4,19 +4,32 @@ import PreviewCamp from './previewCamp';
 const style = {
 	margin: 10,
 	width: "30%",
+	height: 700,
 	display: "inline-block",
-	"vertical-align": "top"
+	"vertical-align": "top",
+	overflow: "scroll"
 };
 
 class RoutePreview extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.scrollToBottom = this.scrollToBottom.bind(this);
+	}
+
+	componentDidUpdate() {
+		this.scrollToBottom();
+	}
+
+	scrollToBottom() {
+  		const scrollBox = this.refs.scrollWindow;
+  		scrollBox.scrollTop = scrollBox.offsetHeight;
 	}
 
 	render() {
 		return (
-			<section style={style}> 
+			<section style={style} ref="scrollWindow"> 
 				{
 					this.props.route.map(
 						(camp, position) => <PreviewCamp camp={camp} number={position+1} />
