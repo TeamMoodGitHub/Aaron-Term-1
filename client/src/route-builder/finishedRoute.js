f541639ace8a0b739407e0d9c10468b9d997de4b
Aaron Kau <aaron.kau@rutgers.edu>
2018-01-05 15:58:10 -0500

    Add Progress Bar.

--- client/src/route-builder/finishedRoute.js
+++ client/src/route-builder/finishedRoute.js
@@ -4,6 +4,20 @@ class FinishedRoute extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.doneLoading = this.doneLoading.bind(this);
+	}
+
+	doneLoading() {
+		this.refs.progress.style.display = "none";
+		this.refs.image.style.display="block";
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.routeImage !== this.props.routeImage && this.refs.progress) {
+			this.refs.progress.style.display="block";
+			this.refs.image.style.display="none";
+		}
 	}
 
 	render() {
@@ -12,7 +26,12 @@ class FinishedRoute extends React.Component {
 				<h1>Finished Route: { JSON.stringify(this.props.routeImage) }</h1>
 				{ 
 					this.props.routeImage ? 
-					(<img src={this.props.routeImage} />) 
+					(
+						<div>
+							<div ref="progress" className="loader" style={{display:"block"}}></div>
+							<img ref="image" src={this.props.routeImage} onLoad={this.doneLoading} style={{display:"none", width: "80%", margin: "20px auto"}}/>
+						</div>
+					) 
 					: 
 					<img /> 
 				}
