9c78ed293410ea36f3851d4e6afd95de9936ff5a
Aaron Kau <aaron.kau@rutgers.edu>
2018-01-11 15:30:23 -0500

    Initial Commit for Arrow Drawings.

--- routeBuilder.js
+++ routeBuilder.js
@@ -35,6 +35,9 @@ async function addRoute(lenna, route) {
 	for (var i = 0 ; i < route.length ; i++) {
 		//console.log("Adding " + route[i].name + (route[i].smite ? " with smite" : "") +" to image.");
 		lenna = await addCamp(lenna, route[i], i);
+		if (i > 0) {
+			lenna = await drawArrow(lenna, route[i-1], route[i]);
+		}
 	}
 
 
@@ -45,6 +48,37 @@ async function addCamp(lenna, camp, position) {
 
 	const image = await (camp.smite ? Jimp.read("circleNums/smite.png") : Jimp.read("circleNums/" + (position+1) + ".png"));
 	return lenna.composite(image, camp.x, camp.y);
+}
+
+async function drawArrow(lenna, camp1, camp2) {
+	const image = await Jimp.read("redArrow.png");
+
+	const deltaX = camp2.x - camp1.x;
+	const deltaY = camp2.y - camp1.y;
+
+	console.log(deltaX + " " + deltaY);
+
+	const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY) - 70;
+	const angle = (deltaX > 0 ? Math.atan(deltaY / deltaX) : -Math.PI + Math.atan(deltaY / deltaX)) * 180 / Math.PI;
+
+	console.log(distance);
+
+	const resizedImage = await image.resize(distance, 25);
+	const arrow = await resizedImage.rotate(angle);
+
+	const adjX = 35 + Math.abs(35 * Math.cos(angle * Math.PI / 180));
+	const adjY = 35 + Math.abs(35 * Math.sin(angle * Math.PI / 180));
+
+	if (angle >= -270 && angle < -180) {
+		return lenna.composite(arrow, camp2.x + adjX, camp1.y + adjY);
+	} else if (angle >= -180 && angle < -90) {
+		return lenna.composite(arrow, camp2.x + adjX, camp2.y + adjY);
+	} else if (angle >= -90 && angle < 0) {
+		return lenna.composite(arrow, camp1.x + adjX, camp2.y + adjY);
+	} else {
+		return lenna.composite(arrow, camp1.x + adjX, camp1.y + adjY);
+	}
+
 }
 
 function getCamp(campString) {
